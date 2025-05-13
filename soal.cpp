32b38a23c538cf3bc973d417e1348de5731f34c4
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-05-13 15:50:29 +0700

    1(1)

---
+++ soal.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//deklarasi bangun datar
+class BangunDatar;
+
+class PersegiPanjang{
+    public:
+    void inputData(BangunDatar &bd);
+    void OutputData(BangunDatar &bd);
+};
+
