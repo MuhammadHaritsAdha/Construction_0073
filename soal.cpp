f9456c822abb345373c040467440a5133f2bc781
MuhammadHaritsAdha <mharitsadha@gmail.com>
2025-05-13 15:51:10 +0700

    2(1)

--- soal.cpp
+++ soal.cpp
@@ -10,3 +10,31 @@ class PersegiPanjang{
     void OutputData(BangunDatar &bd);
 };
 
+class BangunDatar{
+    private:
+        float panjang;
+        float lebar;
+        float hitungLuas(){
+            return panjang * lebar;
+        };
+        float hitungKeliling(){
+            return 2 * (panjang + lebar);
+        };
+    public: 
+        //untuk mengakses panjanng dan lebar
+        friend void PersegiPanjang::inputData(BangunDatar &bd);
+        friend void PersegiPanjang::OutputData(BangunDatar &bd);
+};
+
+void PersegiPanjang::inputData(BangunDatar &bd){
+    cout << "Masukkan Panjang = ";
+    cin >> bd.panjang;
+    cout << "Masukkan Lebar = ";
+    cin >> bd.lebar;
+};
+
+void PersegiPanjang::OutputData(BangunDatar &bd){
+    cout << "Luas = " << bd.hitungLuas() << endl;
+    cout << "Keliling = " << bd.hitungKeliling() << endl;
+};
+
